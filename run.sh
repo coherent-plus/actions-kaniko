595b36aa86b9e0e6d743c70b4d6c3a2ce4e056ad
Jean Soon <jeansoon@gmail.com>
2023-12-14 10:46:23 +0800

    fine tune the build script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker build --platform=linux/amd64 --build-arg tag=${KANIKO_TAG} -t ${KANIKO_CUSTOM_IMAGE}:${KANIKO_TAG} . && \
     docker push ${KANIKO_CUSTOM_IMAGE}:${KANIKO_TAG} && \
     \
-    VALUE="${KANIKO_CUSTOM_IMAGE}:${KANIKO_TAG}" yq -i ".runs.image=strenv(VALUE)" action.yml
+    VALUE="docker://${KANIKO_CUSTOM_IMAGE}:${KANIKO_TAG}" yq -i ".runs.image=strenv(VALUE)" action.yml
